41533384a0fcb62438b0a339a23444d4b4d4f99e
santii-ez <santiago_garciagarcia@outlook.com>
2022-05-20 13:01:36 -0300

    Formularios terminados

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.listen(port, () => {
     console.log('Server runing on port ' + port);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
 app.get('/home', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
